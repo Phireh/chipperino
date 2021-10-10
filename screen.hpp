03881d72c90d7614fadb1d608eb6515edd3799a1
Phireh <Phireh@fukurokuju.dev>
2021-10-11 01:00:08 +0200

    Drawing optimizations

--- screen.hpp
+++ screen.hpp
@@ -11,39 +11,52 @@
 
 #define DISPLAY_START "\033[2;2H"
 
+char screen_buffer[chip8_display_height+2][chip8_display_width+3];
+
 void print_border()
 {    
-    printf(RESET_CURSOR);
     // top border
-    printf("/");
-    printf("%s", std::string(chip8_display_width, '-').c_str());
-    printf("\\" NEXT_LINE);    
+    screen_buffer[0][0] = '/';
+    for (int i = 1; i <= chip8_display_width; ++i)
+      screen_buffer[0][i] = '-';
+    
+    screen_buffer[0][chip8_display_width+1] = '\\';
     // middle
-    for (int i = 0; i < chip8_display_height; ++i)
+    for (int i = 1; i <= chip8_display_height; ++i)
     {
         printf("|%*s|" NEXT_LINE, chip8_display_width, "");
+        screen_buffer[i][0] = '|';
+        screen_buffer[i][chip8_display_width+1] = '|';
     }
     // bottom border
-    printf("\\");
-    printf("%s", std::string(chip8_display_width, '-').c_str());
-    printf("/");
+    screen_buffer[chip8_display_height+1][0] = '\\';
+    for (int i = 1; i <= chip8_display_width; ++i)
+      screen_buffer[chip8_display_height+1][i] = '-';
+    screen_buffer[chip8_display_height+1][chip8_display_width+1] = '//';
 }
 
 void draw_display(chip8_t *c = &chip8)
 {
     print_border();
-    printf(DISPLAY_START);    
 
     for (int i = 0; i < chip8_display_height; ++i)
     {
         for (int j = 0; j < chip8_display_width; ++j)
         {
             char glyph = c->display[i][j] ? '*' : ' ';
-            printf("%c", glyph);
+            screen_buffer[i+1][j+1] = glyph;
         }
-        printf(NEXT_LINE);
-        printf(NEXT_POS);
     }
+
+    // Put newlines just so we can print everything just in a single printf call
+    for (int i = 0; i < chip8_display_height+1; ++i)
+      screen_buffer[i][chip8_display_width+2] = '\n';
+
+    // And the null terminator for the right bottom corner
+    screen_buffer[chip8_display_height+1][chip8_display_width+2] = '\0';
+
+    // finally, the actual printing to screen
+    printf(RESET_CURSOR "%s", screen_buffer);
 }
 
 void clear_screen()
